991414f85ec19f137f5b7a01cdb7c7ae44960579
3loomin <3loomin@gmail.com>
2022-05-18 19:09:59 +0900

    10773번 스택문제 해결

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <stdio.h>
+#if 1
+using namespace std;
+int stack[100001];
+int n , sum, tmp = 0;
+int top = -1;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        scanf("%d",&tmp);
+        if(!tmp){
+            top--;
+        }
+        else{
+            stack[++top] = tmp;
+        }
+    }
+    for(int j = 0; j <= top; j++){
+        sum += stack[j];
+    }
+    cout << sum;
     return 0;
 }
+#endif

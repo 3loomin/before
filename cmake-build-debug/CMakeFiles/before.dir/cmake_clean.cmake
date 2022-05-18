51effd68bf22fefa20dc833cfbc9eeb3e8fc1cdb
3loomin <3loomin@gmail.com>
2022-05-18 18:34:07 +0900

    :sparkles: before repo init

---
+++ cmake-build-debug/CMakeFiles/before.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/before.dir/main.cpp.o"
+  "before"
+  "before.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/before.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
